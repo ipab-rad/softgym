40dff04ec5ae2fe4d3ea2abe8fbd0e98513670fd
Yordan Hristov <yshristov@gmail.com>
2021-01-18 18:05:20 +0000

    reorganise the shell scripts for setting up the softgym environment

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
-. activate softgym
+source /opt/conda/etc/profile.d/conda.sh
+
+conda activate softgym
+
 export PYFLEXROOT=${PWD}/PyFlex
 export PYTHONPATH=${PYFLEXROOT}/bindings/build:$PYTHONPATH
 export LD_LIBRARY_PATH=${PYFLEXROOT}/external/SDL2-2.0.4/lib/x64:$LD_LIBRARY_PATH
+
+./compile_1.0.sh                     
